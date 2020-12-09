ed33c88ea1f1dd94238726866bedf264146c9b05
therealniks <therealniks@gmail.com>
2020-12-09 22:23:17 +0300

    HW3
    some fix HW2. rdy HW3

---
+++ Course2/Course2/Groups/AllGroups/AllGroupsCell.swift
@@ -0,0 +1,16 @@
+//
+//  AllGroupsCell.swift
+//  Course2
+//
+//  Created by N!kS on 09.12.2020.
+//
+
+import UIKit
+
+class AllGroupsCell: UITableViewCell {
+
+    @IBOutlet weak var groupImage: UIImageView!
+    @IBOutlet weak var groupLabel: UILabel!
+    
+
+}
