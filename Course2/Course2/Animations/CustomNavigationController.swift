7c44045b44e7a103e731dfa23ae50a778abcef29
therealniks <therealniks@gmail.com>
2021-01-13 20:01:32 +0300

    some

--- Course2/Course2/Animations/CustomNavigationController.swift
+++ Course2/Course2/Animations/CustomNavigationController.swift
@@ -5,4 +5,50 @@
 //  Created by N!kS on 13.01.2021.
 //
 
-import Foundation
+import UIKit
+
+class CustomNavigationController: UINavigationController {
+    var interactiveTransition = InteractiveManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        delegate = self
+    }
+    
+    func navigationController(_ navigationController: UINavigationController,
+                              interactionControllerFor animationController: UIViewControllerAnimatedTransitioning)
+                              -> UIViewControllerInteractiveTransitioning? {
+        return interactiveTransition.interactionInProgress ? interactiveTransition : nil
+    }
+}
+
+extension CustomNavigationController: UINavigationControllerDelegate {
+    
+    func navigationController(
+        _ navigationController: UINavigationController,
+        animationControllerFor operation: UINavigationController.Operation,
+        from fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        if operation == .push {
+            self.interactiveTransition.viewController = toVC
+            return PushAnimator()
+        } else if operation == .pop {
+            if navigationController.viewControllers.first != toVC {
+                self.interactiveTransition.viewController = toVC
+            }
+            return PopAnimator()
+        }
+        return nil
+    }
+}
+extension CustomNavigationController: UIViewControllerTransitioningDelegate {
+    
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return PopAnimator()
+    }
+    
+    func animationController(forPresented presented: UIViewController,
+                             presenting: UIViewController,
+                             source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return PushAnimator()
+    }
+}
