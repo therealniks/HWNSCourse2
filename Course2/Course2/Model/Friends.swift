b8cf8a528a936e6c994c6284e4d5a97d81250c34
therealniks <therealniks@gmail.com>
2021-01-21 19:46:36 +0300

    homework for less3

---
+++ Course2/Course2/Model/Friends.swift
@@ -0,0 +1,20 @@
+//
+//  Friends.swift
+//  Course2
+//
+//  Created by N!kS on 20.01.2021.
+//
+
+import Foundation
+import SwiftyJSON
+
+struct Friends {
+    let firstName: String
+    var lastName : String
+    let icon: String
+    init(_ json: JSON) {
+        self.firstName = json["first_name"].stringValue
+        self.lastName = json["last_name"].stringValue
+        self.icon = json["photo_100"].stringValue
+    }    
+}
